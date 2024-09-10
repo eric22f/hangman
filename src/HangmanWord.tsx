173cbe8de19aed1d9425286a39c5c21633b5afe1
Eric Faust <eric22f@gmail.com>
2024-09-10 16:23:55 -0700

    added HangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -2,14 +2,17 @@
 function HangmanWord(props: { word: string, guessedLetters: string[] }) {
     const { word, guessedLetters } = props;
     return (
-        <div>
-            {word.split('').map((letter, index) => {
-                if (guessedLetters.includes(letter)) {
-                    return <span key={index}>{letter}</span>;
-                } else {
-                    return <span key={index}>_</span>;
-                }
-            })}
+        <div style={{ display: "flex", gap: ".25em", fontSize: "6rem",
+            fontWeight: "bold", color: "black",
+            textTransform: "uppercase", fontFamily: "monospace"
+        }}>
+            {word.split("").map((letter, index) => (
+                <span key={index} style={{ borderBottom: ".1em solid black"}}>
+                    <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>
+                        {letter}
+                    </span>
+                </span>
+            ))}
         </div>
     );
 }
