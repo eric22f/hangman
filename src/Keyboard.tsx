4ddd39281b173e3202de1919225b6030f9267c0a
Eric Faust <eric22f@gmail.com>
2024-09-10 16:47:23 -0700

    Initial keyboard component

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import styles from './Keyboard.module.css';
 
 function Keyboard() {
-  return /*#__PURE__*/React.createElement("div", {
-    style: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      gap: '1rem',
-      justifyContent: 'center'
-    }
-  }, 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => /*#__PURE__*/React.createElement("button", {
-    key: letter,
-    style: {
-      padding: '1rem',
-      fontSize: '1.5rem',
-      border: 'none',
-      borderRadius: '0.5rem',
-      backgroundColor: 'lightblue'
-    }
-  }, letter)));
-}
+  const KEYS = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  return (
+    <div style={{ alignSelf: "stretch"}}>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(70px, 1fr))",
+        gap: "1em"
+      }}>
+        {KEYS.map(letter => (
+          <button className={styles.btn} key={letter}>{letter}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Keyboard;
