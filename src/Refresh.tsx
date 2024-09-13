0d6e353d606fc6c696dc20ce44f17a6a0cb574a1
Eric Faust <eric22f@gmail.com>
2024-09-13 10:53:41 -0700

    Css update to refresh button to better match the site

--- src/Refresh.tsx
+++ src/Refresh.tsx
@@ -2,7 +2,9 @@
 function Refresh() {
     // Button to refresh the page
     return (
-        <button style={{ height: '2rem', display: 'inline-block', verticalAlign: 'middle', fontSize: '1rem', cursor: 'pointer' }}
+        <button style={{ height: '2rem', display: 'inline-block', verticalAlign: 'middle',
+            fontSize: '1rem', cursor: 'pointer', padding: '0 1rem', backgroundColor: 'white',
+            border: '1px solid black', borderRadius: '5px', color: 'black'}}
          onClick={() => window.location.reload()}>Refresh</button>
     )
 }
